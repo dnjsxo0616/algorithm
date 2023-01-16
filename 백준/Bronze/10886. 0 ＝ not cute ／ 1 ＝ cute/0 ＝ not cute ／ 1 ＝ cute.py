4d03226660deee1700bd721ec2b29e29d3260276
WonTae Han <57781744+dnjsxo0616@users.noreply.github.com>
2023-01-16 17:33:41 +0900

    [Bronze III] Title: 0 = not cute / 1 = cute, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/10886. 0 ＝ not cute ／ 1 ＝ cute/0 ＝ not cute ／ 1 ＝ cute.py
+++ 백준/Bronze/10886. 0 ＝ not cute ／ 1 ＝ cute/0 ＝ not cute ／ 1 ＝ cute.py
@@ -1,19 +1,10 @@
 N = int(input())
-ans = []
-o = 0
-x = 0
+ans = 0
 
 for i in range(N):
     a = int(input())
-    ans.append(a)
-
-for j in ans:
-    if j == 1:
-        o += 1
-    elif j == 0:
-        x +=1
-
-if o>x:
-    print("Junhee is cute!")
-else:
+    ans += a
+if ans<(N-ans):   
     print("Junhee is not cute!")
+else:
+    print("Junhee is cute!")
