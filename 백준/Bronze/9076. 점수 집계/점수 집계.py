67e7e5be039400f2b0608caf7996107978e9f6dd
WonTae Han <57781744+dnjsxo0616@users.noreply.github.com>
2023-02-01 15:02:06 +0900

    [Bronze II] Title: 점수 집계, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/9076. 점수 집계/점수 집계.py
@@ -0,0 +1,10 @@
+T = int(input())
+
+for t in range(T):
+    score = list(map(int, input().split()))
+    score.remove(max(score))
+    score.remove(min(score))
+    if max(score)-min(score) >= 4:
+        print('KIN')
+    else:
+        print(sum(score))
