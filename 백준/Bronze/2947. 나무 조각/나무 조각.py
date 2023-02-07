fe566b2ad151bdc2560d57efd48ede82583bc64f
WonTae Han <57781744+dnjsxo0616@users.noreply.github.com>
2023-02-07 17:57:08 +0900

    [Bronze I] Title: 나무 조각, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2947. 나무 조각/나무 조각.py
+++ 백준/Bronze/2947. 나무 조각/나무 조각.py
@@ -1,16 +1,8 @@
-num = list(map(int, input().split()))
-while True:
-    if num[0] > num[1]:
-        num[0], num[1] = num[1], num[0]
-        print(*num)
-    if num[1] > num[2]:
-        num[1], num[2] = num[2], num[1]
-        print(*num)
-    if num[2] > num[3]:
-        num[2], num[3] = num[3], num[2]
-        print(*num)
-    if num[3] > num[4]:
-        num[3], num[4] = num[4], num[3]
-        print(*num)
-    if num == [1, 2, 3, 4, 5]:
-        break
+nums = list(map(int, input().split()))
+arr = [1, 2, 3, 4, 5]
+
+while nums != arr:
+    for i in range(len(nums)-1):
+        if nums[i] > nums[i+1]:
+            nums[i], nums[i+1] = nums[i+1], nums[i]
+            print(*nums)
