1fa3a8f9cd3cde58d6e87cb0ba34c7dc10c54b0e
WonTae Han <57781744+dnjsxo0616@users.noreply.github.com>
2023-02-09 16:39:32 +0900

    [Silver III] Title: 문자열 집합, Time: 908 ms, Memory: 36788 KB -BaekjoonHub

--- 백준/Silver/14425. 문자열 집합/문자열 집합.py
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.py
@@ -1,13 +1,14 @@
 n, m = map(int, input().split())
 
-to_s = []
+s = []
 
 for _ in range(n):
     a = input()
-    to_s.append(a)
+    s.append(a)
+S = set(s)
 cnt = 0
 for _ in range(m):
     t = input()
-    if t in to_s:
+    if t in S:
         cnt += 1
 print(cnt)
