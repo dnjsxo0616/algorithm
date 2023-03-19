e523f5b87d15c39a25de442dec1a111e873ab261
WonTae Han <57781744+dnjsxo0616@users.noreply.github.com>
2023-03-19 23:57:28 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.py
@@ -0,0 +1,10 @@
+def solution(num_list):
+    even_num = 0
+    odd_num = 0
+    for i in num_list:
+        if i%2 == 0:
+            even_num += 1
+        else:
+            odd_num += 1
+    answer = [even_num, odd_num]
+    return answer
