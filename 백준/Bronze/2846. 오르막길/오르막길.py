71a063a649b4736b11e3c2cb682ce9a608db7b15
WonTae Han <57781744+dnjsxo0616@users.noreply.github.com>
2023-04-18 00:37:57 +0900

    [Bronze I] Title: 오르막길, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2846. 오르막길/오르막길.py
+++ 백준/Bronze/2846. 오르막길/오르막길.py
@@ -1,16 +1,13 @@
 n = int(input())
+arr = list(map(int, input().split()))
 
-numbers = list(map(int, input().split()))
-
-ans = [numbers[0]]
-res = []
-for i in numbers:
-    if i > ans[-1]:
-        ans.append(i)
-    elif i <= ans[-1]:
-        # ans의 오르막길 크기를 따로 저장한 후 ans를 초기화한 후 i를  넣어준다.
-        res.append(ans[-1]-ans[0])
-        ans.clear()
-        ans.append(i)
-res.append(ans[-1]-ans[0])
-print(max(res))
+rep = []
+ans = 0
+for i in range(n-1): # 0 1 2 3 4
+    if arr[i] < arr[i+1]:
+        ans += arr[i+1] - arr[i]
+    else:
+        rep.append(ans)
+        ans = 0
+rep.append(ans)
+print(max(rep))
