328b5a9533cf3297ca8e6a59a258f722ff93b166
WonTae Han <57781744+dnjsxo0616@users.noreply.github.com>
2023-07-04 22:49:22 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.09 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.py
+++ 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.py
@@ -1,13 +1,7 @@
 def solution(array):
-    answer = {}
-    for i in array:
-        if i not in answer:
-            answer[i] = 1
-        else:
-            answer[i] += 1
-    res = [k for k,v in answer.items() if max(answer.values()) == v]
-    if len(res) > 1:
-        a = -1
-    else:
-        a = res[0]
-    return a
+    while len(array) != 0:
+        for i, a in enumerate(set(array)):
+            print(array)
+            array.remove(a)
+        if i == 0: return a
+    return -1
