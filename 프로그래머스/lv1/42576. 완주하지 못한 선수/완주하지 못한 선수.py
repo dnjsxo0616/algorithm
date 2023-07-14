c6bb9e245790dfd558b98b1ace14b5545aa657ab
WonTae Han <57781744+dnjsxo0616@users.noreply.github.com>
2023-07-14 22:43:40 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 96.22 ms, Memory: 39 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,18 +1,4 @@
+import collections
 def solution(participant, completion):
-    answer = []
-    part = {}
-    for i in participant:
-        if i in part:
-            part[i] += 1
-        else:
-            part[i] = 1
-    com = {}
-    for i in completion:
-        if i in com:
-            com[i] += 1
-        else:
-            com[i] = 1
-    for key in part:
-        if key not in com or part[key] != com[key]:
-            answer = key
-    return answer
+    answer = collections.Counter(participant) - collections.Counter(completion)
+    return list(answer.keys())[0]
