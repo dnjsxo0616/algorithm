51634cf766fc9b883ba247f155c5ce020c742b10
WonTae Han <57781744+dnjsxo0616@users.noreply.github.com>
2023-07-16 22:59:15 +0900

    [level 0] Title: 공 던지기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120843. 공 던지기/공 던지기.py
+++ 프로그래머스/lv0/120843. 공 던지기/공 던지기.py
@@ -1,6 +1,2 @@
 def solution(numbers, k):
-    answer = 0
-    if len(numbers) < k*2:
-        numbers = numbers * ((k*2) // len(numbers) + 1)
-    answer = numbers[2*(k-1)]
-    return answer
+    return numbers[2 * (k - 1) % len(numbers)]
