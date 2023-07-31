63a497fff127eb7748ec5936938ee00d94d6d8ee
WonTae Han <57781744+dnjsxo0616@users.noreply.github.com>
2023-07-31 13:04:12 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 63.47 ms, Memory: 39 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,17 +1,5 @@
+import collections
+
 def solution(participant, completion):
-    res = ''
-    answer = {}
-    for i in participant:
-        if i not in answer:
-            answer[i] = 1
-        else:
-            answer[i] += 1
-    
-    for i in completion:
-        if i in answer:
-            answer[i] -= 1
-    
-    for k,v in answer.items():
-        if v == 1:
-            res += k
-    return res
+    answer = collections.Counter(participant) - collections.Counter(completion)
+    return list(answer.keys())[0]
