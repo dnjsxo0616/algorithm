7386f1a9d4392130a7630e8b6fb41aacf6e20a57
WonTae Han <57781744+dnjsxo0616@users.noreply.github.com>
2023-07-31 13:15:48 +0900

    [level 2] Title: 의상, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42578. 의상/의상.py
+++ 프로그래머스/lv2/42578. 의상/의상.py
@@ -2,14 +2,10 @@ def solution(clothes):
     res = {}
     answer = 1
     
-    for i in clothes:
-        if i[1] in res:
-            res[i[1]] += 1
-        else:
-            res[i[1]] = 1
-
-    for k, v in res.items():
-        con = v + 1
-
-        answer *= con
+    for clothe, types in clothes:
+        res[types] = res.get(types, 0) +1
+    
+    for i in res:
+        answer *= (res[i] + 1)
+        
     return answer-1
