0cc72868b52fb4f55a970ca95b5a92ba64ec6555
WonTae Han <57781744+dnjsxo0616@users.noreply.github.com>
2023-08-03 17:44:32 +0900

    [level 0] Title: 암호 해독, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120892. 암호 해독/암호 해독.py
+++ 프로그래머스/lv0/120892. 암호 해독/암호 해독.py
@@ -1,6 +1,2 @@
 def solution(cipher, code):
-    answer = ''
-    for i in range(len(cipher)):
-        if (i+1)%code == 0 :
-            answer += cipher[i]
-    return answer
+    return cipher[code-1::code]
