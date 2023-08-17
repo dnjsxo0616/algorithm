0b35cb9f2c05fc2267cade365f26057dd923fefe
WonTae Han <57781744+dnjsxo0616@users.noreply.github.com>
2023-08-18 00:32:55 +0900

    [level 2] Title: 전화번호 목록, Time: 98.31 ms, Memory: 28.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,12 +1,9 @@
 def solution(phone_book):
     answer = True
-    hash_map = {}
-    for phone_number in phone_book:
-        hash_map[phone_number] = 1
-    for phone_number in phone_book:
-        temp = ""
-        for number in phone_number:
-            temp += number
-            if temp in hash_map and temp != phone_number:
-                answer = False
+    
+    phone_book.sort()
+    for i in range(len(phone_book)-1):
+        if phone_book[i] == phone_book[i+1][:len(phone_book[i])]:
+            answer = False
+        
     return answer
