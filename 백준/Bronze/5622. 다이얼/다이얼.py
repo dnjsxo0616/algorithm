fa9fa2691b63316037e604bd3455a83fa97c6ac8
WonTae Han <57781744+dnjsxo0616@users.noreply.github.com>
2023-09-02 01:22:28 +0900

    [Bronze II] Title: 다이얼, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/5622. 다이얼/다이얼.py
+++ 백준/Bronze/5622. 다이얼/다이얼.py
@@ -1,18 +1,13 @@
-n = input()
-list_n = list(n)
-ans = {
-    3:['A', 'B', 'C'],
-    4:['D', 'E', 'F'],
-    5:['G', 'H', 'I'],
-    6:['J', 'K', 'L'],
-    7:['M', 'N', 'O'],
-    8:['P', 'Q', 'R', 'S'],
-    9:['T', 'U', 'V'],
-    10:['W', 'X', 'Y', 'Z']
-}
-cnt = 0
-for word in list_n:
-    for k, v in ans.items():
-        if word in v:
-            cnt += k
-print(cnt)
+import sys
+input = sys.stdin.readline
+
+diary = ['ABC', 'DEF', 'GHI', 'JKL', 'MNO', 'PQRS', 'TUV', 'WXYZ']
+
+string = input()
+time = 0
+
+for i in string:
+    for j in diary:
+        if i in j:
+            time += diary.index(j) + 3
+print(time)
