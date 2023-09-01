ed8439e1454c6c944c4137971c8eb5a6bb216505
WonTae Han <57781744+dnjsxo0616@users.noreply.github.com>
2023-09-02 01:48:49 +0900

    [Bronze II] Title: 피시방 알바, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/1453. 피시방 알바/피시방 알바.py
+++ 백준/Bronze/1453. 피시방 알바/피시방 알바.py
@@ -1,10 +1,17 @@
-N = int(input())
-num = []
-re = []
-cus = input().split()
-for i in cus:
-    if i not in num:
-        num.append(i)
-    elif i in num:
-        re.append(i)
-print(len(re))
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+seat = []
+answer = []
+
+nums = list(input().split())
+
+for i in nums:
+    if i not in seat:
+        seat.append(i)
+    else:
+        answer.append(i)
+
+print(len(answer))
