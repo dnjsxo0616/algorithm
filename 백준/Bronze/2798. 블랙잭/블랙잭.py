a627993e3191db231904dbd4c7eba3c3e902643a
WonTae Han <57781744+dnjsxo0616@users.noreply.github.com>
2023-10-04 01:08:44 +0900

    [Bronze II] Title: 블랙잭, Time: 88 ms, Memory: 37628 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.py
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -1,11 +1,13 @@
-n, m = map(int, input().split())
+n, m = list(map(int, input().split()))
+num = list(map(int, input().split()))
 
-numbers = list(map(int, input().split()))
+#3장씩 m이 넘지않는 최대수
 ans = []
+
 for i in range(n-2):
-    for j in range(i+1,n-1):
+    for j in range(i+1, n-1):
         for k in range(j+1, n):
-           sun = numbers[i] + numbers[j] + numbers[k]
-           if sun <= m:
-               ans.append(sun)
+            total = num[i] + num[j] + num[k]
+            if total <= m:
+                ans.append(total)
 print(max(ans))
