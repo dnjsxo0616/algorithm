7aa38b20d450c53ec68a861c8cab8cac65fbc97e
WonTae Han <57781744+dnjsxo0616@users.noreply.github.com>
2023-11-19 18:44:10 +0900

    [level unrated] Title: 등차수열의 특정한 항만 더하기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.py
+++ 프로그래머스/unrated/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.py
@@ -1,11 +1,8 @@
 def solution(a, d, included):
     numbers = [a]
     answer = 0
-
-    for i in range(len(included)-1):
-        numbers.append(numbers[i]+d)
         
     for i in range(len(included)):
         if(included[i] == True):
-            answer += numbers[i]
+            answer += d*i+a
     return answer
