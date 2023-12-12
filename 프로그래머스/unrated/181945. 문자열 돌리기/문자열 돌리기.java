dfd375c62a63308d14b0495200a0709a12e6a0ce
WonTae Han <57781744+dnjsxo0616@users.noreply.github.com>
2023-12-12 17:55:36 +0900

    [level unrated] Title: 문자열 돌리기, Time: 126.06 ms, Memory: 66.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181945. 문자열 돌리기/문자열 돌리기.java
+++ 프로그래머스/unrated/181945. 문자열 돌리기/문자열 돌리기.java
@@ -4,9 +4,8 @@ public class Solution {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String a = sc.next();
-        String[] numbers = a.split("");
-        for(int i=0; i<numbers.length; i++) {
-            System.out.println(numbers[i]);
+        for(char c : a.toCharArray()) {
+            System.out.println(c);
         }
     }
 }
