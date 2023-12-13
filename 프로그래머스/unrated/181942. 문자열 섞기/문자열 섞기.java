1e5aa1c0489ce712a281a2dd59d4fa6eec514682
WonTae Han <57781744+dnjsxo0616@users.noreply.github.com>
2023-12-13 16:01:45 +0900

    [level unrated] Title: 문자열 섞기, Time: 14.39 ms, Memory: 76.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181942. 문자열 섞기/문자열 섞기.java
+++ 프로그래머스/unrated/181942. 문자열 섞기/문자열 섞기.java
@@ -1,7 +1,7 @@
 class Solution {
     public String solution(String str1, String str2) {
         String answer = "";
-        for(int i=0; i<str1.length(); i++){
+        for(int i=0; i<str1.length(); i++) {
             answer += str1.charAt(i);
             answer += str2.charAt(i);
         }
