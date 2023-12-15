da72a045329cef7879c2a71040a765561f6ee2c7
WonTae Han <57781744+dnjsxo0616@users.noreply.github.com>
2023-12-15 23:44:28 +0900

    [level unrated] Title: 더 크게 합치기, Time: 1.13 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.java
+++ 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.java
@@ -1,11 +1,9 @@
 class Solution {
     public int solution(int a, int b) {
-        String sum1 = String.valueOf(a) + String.valueOf(b);
-        String sum2 = String.valueOf(b) + String.valueOf(a);
-        if(Integer.parseInt(sum1) > Integer.parseInt(sum2)){
-            return Integer.parseInt(sum1);
-        } else {
-            return Integer.parseInt(sum2);
-        }
+
+        int result1 = Integer.parseInt(String.valueOf(a) + String.valueOf(b));
+        int result2 = Integer.parseInt(String.valueOf(b) + String.valueOf(a));
+        
+        return result1 > result2 ? result1 : result2;
     }
 }
