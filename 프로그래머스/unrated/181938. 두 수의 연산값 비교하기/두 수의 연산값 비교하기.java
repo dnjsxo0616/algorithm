62170aaa0301fa931fa9fb2aaad9f7edd30f43c3
WonTae Han <57781744+dnjsxo0616@users.noreply.github.com>
2023-12-16 21:25:44 +0900

    [level unrated] Title: 두 수의 연산값 비교하기, Time: 10.30 ms, Memory: 72.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181938. 두 수의 연산값 비교하기/두 수의 연산값 비교하기.java
+++ 프로그래머스/unrated/181938. 두 수의 연산값 비교하기/두 수의 연산값 비교하기.java
@@ -1,11 +1,8 @@
 class Solution {
     public int solution(int a, int b) {
-        int sum = Integer.parseInt(""+a+b);
-        int multiply = 2*a*b;
-        if(sum >= multiply) {
-            return sum;
-        } else {
-            return multiply;
-        }
+        
+        int plus = Integer.parseInt(""+a+b);
+        int multiple = 2 * a * b;
+        return plus > multiple ? plus : multiple;
     }
 }
