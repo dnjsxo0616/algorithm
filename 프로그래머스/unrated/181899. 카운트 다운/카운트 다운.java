48936929a490ca066a9a7bca57e4151a27f74ac3
WonTae Han <57781744+dnjsxo0616@users.noreply.github.com>
2024-01-07 13:48:59 +0900

    [level unrated] Title: 카운트 다운, Time: 0.02 ms, Memory: 72.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181899. 카운트 다운/카운트 다운.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] solution(int start, int end_num) {
+        int a = start-end_num + 1;
+        int[] answer = new int[a];
+        for(int i=0; i<a; i++) {
+            answer[i] = start;
+            start--;
+        }
+        return answer;
+    }
+}
