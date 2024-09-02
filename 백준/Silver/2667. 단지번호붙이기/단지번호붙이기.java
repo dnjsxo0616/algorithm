81ab7e40458e2f0176a2e927cffd25addae2defc
WonTae Han <57781744+dnjsxo0616@users.noreply.github.com>
2024-09-02 16:41:34 +0900

    [Silver I] Title: 단지번호붙이기, Time: 112 ms, Memory: 14192 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -1,62 +1,71 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
-public class Main{
-    static int N, count;
-    static int[][] map;
-    static int[] dx = {-1, 1, 0, 0};
-    static int[] dy = {0, 0, -1, 1};
-    
-    public static void bfs(int x, int y){
-        Queue<int[]> que = new LinkedList<>();
-        que.add(new int[]{x,y});
-        map[x][y] = 0;
-        
-        while(!que.isEmpty()){
-            count += 1;
-            int now[] = que.poll();
-            for(int i=0; i<4; i++){
-                int nx = now[0] + dx[i];
-                int ny = now[1] + dy[i];
-            
-                if(nx>=0 && nx<N && ny>=0 && ny<N && map[nx][ny]==1){
-                    map[nx][ny] = 0;
-                    que.offer(new int[]{nx, ny});
+public class Main {
+    static int n;
+    static int[][] graph;
+    static int[][] apart;
+    static boolean[][] visited;
+    static int[] dx = {1, 0, -1, 0};
+    static int[] dy = {0, 1, 0, -1};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+
+        graph = new int[n][n];
+        visited = new boolean[n][n];
+
+        for(int i = 0; i < n; i++) {
+            String line = br.readLine();
+            for(int j = 0; j < n; j++) {
+                graph[i][j] = line.charAt(j) - '0';
+            }
+        }
+
+        List<Integer> ans = new ArrayList<>();
+        int area = 0;
+
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<n; j++) {
+                if(!visited[i][j] && graph[i][j] == 1) {
+                    int num = BFS(i, j);
+                    ans.add(num);
+                    area++;
                 }
             }
+        }
+
+        System.out.println(area);
+        Collections.sort(ans);
+        for(int n : ans) {
+            System.out.println(n);
         }
     }
-    
-    public static void main(String args[]) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        N = Integer.parseInt(br.readLine());
-        map = new int[N][N];
-        
-        for(int i=0; i<N; i++){
-            String str = br.readLine();
-            for(int j=0; j<N; j++){
-                map[i][j] = str.charAt(j) - '0';
-            }
-        }
-        
-        ArrayList<Integer> arr = new ArrayList<>();
-        int result = 0;
-        for(int i=0; i<N; i++){
-            for(int j=0; j<N; j++){
-                count = 0;
-                if(map[i][j] == 1){
-                    bfs(i,j);
-                    arr.add(count);
-                    result++;
+
+    public static int BFS(int row, int col) {
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[]{row, col});
+        visited[row][col] = true;
+        int count = 0;
+
+        while(!queue.isEmpty()) {
+            count++;
+            int[] p = queue.poll();
+            for(int i=0; i<4; i++) {
+                int nextX = p[1] + dx[i];
+                int nextY = p[0] + dy[i];
+
+                if(nextX >= 0 && nextY >= 0 && nextX < n && nextY < n) {
+                    if(!visited[nextY][nextX] && graph[nextY][nextX] == 1) {
+                        queue.offer(new int[]{nextY, nextX});
+                        visited[nextY][nextX] = true;
+                    }
                 }
             }
         }
-        
-        Collections.sort(arr);
-        System.out.println(result);
-        for(int i : arr){
-            System.out.println(i);
-        }
+        return count;
     }
 }
