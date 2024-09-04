5dac7ddb2978a2554d9a5920ebea6778e91c0a50
WonTae Han <57781744+dnjsxo0616@users.noreply.github.com>
2024-09-04 09:51:28 +0900

    [Silver I] Title: 카드 합체 놀이, Time: 144 ms, Memory: 15232 KB -BaekjoonHub

--- 백준/Silver/15903. 카드 합체 놀이/카드 합체 놀이.java
+++ 백준/Silver/15903. 카드 합체 놀이/카드 합체 놀이.java
@@ -1,31 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-  public static void main(String[] args) {
-      Scanner sc = new Scanner(System.in);
-      int line = sc.nextInt();
-      int turn = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
 
-      PriorityQueue<Long> pQ = new PriorityQueue<>();
+        PriorityQueue<Long> queue = new PriorityQueue<>();
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<n; i++) {
+            queue.offer(Long.parseLong(st.nextToken()));
+        }
 
-      for(int i=0; i<line; i++) {
-          pQ.add(sc.nextLong());
-      }
+        for(int i=0; i<m; i++) {
+            long a = queue.poll();
+            long b = queue.poll();
+            long sum = a + b;
+            queue.offer(sum);
+            queue.offer(sum);
+        }
 
-      for(int j=0;  j<turn; j++) {
-          long q1 = pQ.poll();
-          long q2 = pQ.poll();
-          long sumNum = q1 + q2;
-
-          pQ.add(sumNum);
-          pQ.add(sumNum);
-      }
-      
-      long sum = 0;
-      while(!pQ.isEmpty()) {
-          sum += pQ.poll();
-      }
-
-      System.out.print(sum);
-  }
+        long ans = 0;
+        while(!queue.isEmpty()) {
+            long num = queue.poll();
+            ans += num;
+        }
+        System.out.println(ans);
+    }
 }
