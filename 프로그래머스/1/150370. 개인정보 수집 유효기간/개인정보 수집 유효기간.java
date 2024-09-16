30edeb4a0939655914767cb50b066d7aba614496
WonTae Han <57781744+dnjsxo0616@users.noreply.github.com>
2024-09-16 17:32:55 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 10.96 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -2,61 +2,29 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String today, String[] terms, String[] privacies) {
-        List<Integer> res = new ArrayList<>();
-        
-        String[] t = today.split("\\.");
-        int year = Integer.parseInt(t[0]);
-        int month = Integer.parseInt(t[1]);
-        int day = Integer.parseInt(t[2]);
-        
+        List<Integer> ans = new ArrayList<>();
         Map<String, Integer> map = new HashMap<>();
+        int date = getDate(today);
         
-        for(int i=0; i<terms.length; i++) {
-            String[] ts  = terms[i].split(" ");
-            String type = ts[0];
-            int term = Integer.parseInt(ts[1]);
-            map.put(type, term);
+        for(String term : terms) {
+            String[] t = term.split(" ");
+            map.put(t[0], Integer.parseInt(t[1]));
         }
         
-        //위에 현재보다 날짜가 더 과거면 파기
-        
-        for(int j=0; j<privacies.length; j++) {
-            String[] p = privacies[j].split(" ");
-            String[] term = p[0].split("\\.");
-            int y = Integer.parseInt(term[0]);
-            int m = Integer.parseInt(term[1]);
-            int d = Integer.parseInt(term[2]);
-            
-            int exterm = map.get(p[1]);
-            // System.out.println(exterm);
-            m += exterm;
-            
-        
-            
-            while(m > 12) {
-                y++;
-                m = m - 12;  
-            }
-            // 계한하나 년도가 today보다 적어야살아 많으면 파기야
-            if(year > y) {
-                res.add(j+1);
-                continue;
-            }
-            if(year == y && month > m) {
-                res.add(j+1);
-                continue;
-            }
-            if(year == y && month == m && day > d ) {
-                res.add(j+1);
-                continue;
-            }
-            if(year== y && month == m && day == d) {
-                res.add(j+1);
-                continue;
+        for(int i=0; i<privacies.length; i++) {
+            String[] p = privacies[i].split(" ");
+            if(getDate(p[0]) + (map.get(p[1]) * 28) <= date) {
+                ans.add(i+1);
             }
         }
-        int[] answer = res.stream().mapToInt(Integer::intValue).toArray();
-        
-        return answer;
+        return ans.stream().mapToInt(i -> i).toArray();
+    }
+    
+    private int getDate(String today) {
+        String[] pr = today.split("\\.");
+        int year = Integer.parseInt(pr[0]);
+        int month = Integer.parseInt(pr[1]);
+        int day = Integer.parseInt(pr[2]);
+        return (year * 12 * 28) + (month * 28) + day;
     }
 }
