114594e29e3ce46b53f8cb041f54c11afb68c24d
WonTae Han <57781744+dnjsxo0616@users.noreply.github.com>
2024-10-06 12:28:48 +0900

    [Silver IV] Title: 수 찾기, Time: 1064 ms, Memory: 50964 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,48 +1,49 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
+    static int m;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-
-        int[] nums = new int[n];
-
+        m = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i=0; i<n; i++) {
+
+        int[] nums = new int[m];
+        for (int i = 0; i < m; i++) {
             nums[i] = Integer.parseInt(st.nextToken());
         }
         Arrays.sort(nums);
 
-        int m = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
-        for(int j=0; j<m; j++) {
-            int num = Integer.parseInt(st.nextToken());
-            if((binarySearch(nums, num)) >= 0) {
+        int[] finds = new int[n];
+        for (int i = 0; i < n; i++) {
+            int input = Integer.parseInt(st.nextToken());
+            if(binarySearch(nums, input)) {
                 System.out.println(1);
-            } else {
-                System.out.println(0);
-            }
+            }else System.out.println(0);
         }
     }
-    public static int binarySearch(int[] nums, int key) {
-        int lo = 0;
-        int hi = nums.length - 1;
 
-        while(lo <= hi) {
+    public static boolean binarySearch(int[] nums, int key) {
+        int lo = 0;
+        int hi = nums.length-1;
+
+        while (lo <= hi) {
             int mid = (lo + hi) / 2;
 
             if (key < nums[mid]) {
                 hi = mid - 1;
             } else if (key > nums[mid]) {
                 lo = mid + 1;
-            }
-            else {
-                return mid;
+            } else {
+                return true;
             }
         }
-        return -1;
+        return false;
     }
 }
