8e95a13b9f94f615459c212711f7d47fd36a9939
WonTae Han <57781744+dnjsxo0616@users.noreply.github.com>
2024-11-03 01:57:50 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.03 ms, Memory: 77.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n) {
+        int count = Integer.bitCount(n);
+        while(true) {
+            n++;
+            if(Integer.bitCount(n) == count) {
+                return n;
+            }
+        }
+    }
+}
